3b906ea6e3906d057ad5bbf9705380b6f5c48889
Nazi Mikhelashvili <nazomikhelashvili@gmail.com>
2023-12-31 22:04:52 +0300

    added key parameters types

--- GeekBrains/lesson_1C#/Task1/Program.cs
+++ GeekBrains/lesson_1C#/Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+int value=100;
+Console.WriteLine(value);
+
+double d=5.55;
+Console.WriteLine(d);
+
+string c = "!";
+Console.WriteLine(c);
+
+bool b = true;
+Console.WriteLine(b);
+
